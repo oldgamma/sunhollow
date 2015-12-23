1933bc45a35375b90bd368349e65c8a54f3bb8d1
vagrant <vagrant@localhost.localdomain>
2015-12-23 16:31:59 -0500

    Packages gcc and puts it on the repository.

--- build.sh
+++ build.sh
@@ -1,13 +1,52 @@
 #!/bin/bash
 
-RELEASE=0.1.0 &&
-    rm --recursive --force sunhollow-*.spec gcc-4.8.2* build &&
-    sed -e "s#RELEASE#${RELEASE}#" -e "wsunhollow-${RELEASE}.spec" sunhollow.spec &&
-    wget http://ftp.gnu.org/gnu/gcc/gcc-4.8.2/gcc-4.8.2.tar.bz2 &&
-    bunzip2  gcc-4.8.2.tar.bz2 &&
-    gzip gcc-4.8.2.tar &&
+VERSION=4.8.2 &&
+    RELEASE=0.1.0 &&
+    # clean up from the last build
+    rm --recursive --force build &&
+    # the spec file
+    mkdir build &&
+    sed -e "s#RELEASE#${RELEASE}#" -e "s#VERSION#${VERSION}#" -e "wbuild/sunhollow-${RELEASE}.spec" sunhollow.spec &&
+    # the source file
+    wget http://ftp.gnu.org/gnu/gcc/gcc-${VERSION}/gcc-${VERSION}.tar.bz2 &&
+    bunzip2  gcc-${VERSION}.tar.bz2 &&
+    tar --extract --file gcc-${VERSION}.tar &&
+    rm gcc-${VERSION}.tar &&
+    mv gcc-${VERSION} build/sunhollow-${VERSION} &&
+    tar --create --file build/sunhollow-${VERSION}.tar --directory build sunhollow-${VERSION} &&
+    gzip -9 --to-stdout build/sunhollow-${VERSION}.tar > build/sunhollow-${VERSION}.tar.gz &&
+    # the source rpm
     mkdir --parents build/init/01 &&
     mock --init --resultdir build/init/01 &&
     mkdir --parents build/buildsrpm/01 &&
-    mock --buildsrpm --spec sunhollow-${RELEASE}.spec --sources gcc-4.8.2.tar.gz --resultdir build/buildsrpm/01 &&
+    mock --buildsrpm --spec build/sunhollow-${RELEASE}.spec --sources build/sunhollow-${VERSION}.tar.gz --resultdir build/buildsrpm/01 &&
+    # the binary rpm
+    mkdir --parents build/init/02 &&
+    mock --init --resultdir build/init/02 &&
+    mkdir --parents build/rebuild/01 &&
+    mock --rebuild build/buildsrpm/01/sunhollow-${VERSION}-${RELEASE}.src.rpm --resultdir build/rebuild/01 &&
+    # test it
+    mkdir --parents build/init/03 &&
+    mock --init --resultdir build/init/03 &&
+    mkdir --parents build/install/01 &&
+    mock --install build/rebuild/01/sunhollow-${VERSION}-${RELEASE}.x86_64.rpm &&
+    # these are all the packages I think we need for cloud9
+    for PACKAGE in git libxml2-devel libjpeg-devel python gcc-c++ make openssl-devel gcc ruby ruby-devel rubygems tree nodejs npm ncurses ncurses-devel glibc-static
+    do
+	mkdir --parents build/install/02/${PACKAGE} &&
+	    mock --install ${PACKAGE} --resultdir build/install/02/${PACKAGE} &&
+	    true
+    done &&
+    mkdir --parents build/shell/01 &&
+    mock --shell "export PATH=/opt/gcc/bin:\${PATH} && git clone git://github.com/c9/core.git c9sdk && cd c9sdk && scripts/install-sdk.sh && true" --resultdir build/shell/01 &&
+    # put it in the github repo
+    git clone git@github.com:rawflag/dancingleather.git build/dancingleather &&
+    cp build/rebuild/01/sunhollow-${VERSION}-${RELEASE}.x86_64.rpm build/dancingleather &&
+    cp build/buildsrpm/01/sunhollow-${VERSION}-${RELEASE}.src.rpm build/dancingleather &&
+    cd build/dancingleather &&
+    git checkout issue-0001-dancingleather &&
+    createrepo . &&
+    git add sunhollow-${VERSION}-${RELEASE}.x86_64.rpm sunhollow-${VERSION}-${RELEASE}.src.rpm repodata &&
+    git commit -am "Adding sunhollow ${VERSION} ${RELEASE}" -S &&
+    git push origin issue-0001-dancingleather &&
     true
--- sunhollow.spec
+++ sunhollow.spec
@@ -1,4 +1,4 @@
-Summary: This is gcc-4.8.2.  Because the versions of gcc that are available to fedora 23 don't work with the versions of ncurses that are available to fedora 23 and this one does.
+Summary: This is gcc.  Because the versions of gcc that are available to fedora 23 don't work with the versions of ncurses that are available to fedora 23 and this one does.  This is specifically for the purpose of building cloud9.  Do not use it for other purposes.  (It hogs /usr/local/sbin.)
 Name: sunhollow
 Version: 4.8.2
 Release: 0.1.0
@@ -14,39 +14,43 @@ BuildRequires: glibc-devel
 BuildRequires: flex
 BuildRequires: bison
 BuildRequires: glibc-static
+BuildRequires: zlib-devel
 
 %description
-Back version of gcc because of gcc/ncurses incompatibility.
+Back version of gcc because of gcc/ncurses incompatibility.  Use this to build cloud9.  This should only be used in BuildRequires because it selfishly hogs /usr/local/sbin.
 
 %prep
 %setup -q
+%global debug_package %{nil}
 
 %build
 mkdir build &&
+echo BUILD &&
+ls -lh &&
+ls -lh INSTALL &&
+cat README &&
 cd build &&
 ../configure --disable-checking --enable-languages=c,c++ \
 --enable-multiarch --enable-shared --enable-threads=posix \
 --program-suffix=4.8 --with-gmp=/usr/local/lib --with-mpc=/usr/lib \
 --with-mpfr=/usr/lib --without-included-gettext --with-system-zlib \
 --with-tune=generic \
---prefix=${RPM_BUILD_ROOT}/usr/install/gcc-4.8.2 \
+--prefix=/opt/gcc \
 --disable-multilib &&
 make -j8 &&
 true
 
-
 %install
 rm -rf ${RPM_BUILD_ROOT} &&
-mkdir --parents ${RPM_BUILD_ROOT}/usr/install/gcc-4.8.2 &&
-make install &&
-ln --symbolic --force ${RPM_BUILD_ROOT}/usr/install/gcc-4.8.2/bin/gcc4.8 ${RPM_BUILD_ROOT}/usr/bin/gcc &&
+mkdir --parents ${RPM_BUILD_ROOT}/opt/gcc &&
+cd build &&
+make DESTDIR=${RPM_BUILD_ROOT} install &&
+export EXE=${RPM_BUILD_ROOT}/opt/gcc/bin/gccVERSION &&
+cp ${EXE%.*} ${RPM_BUILD_ROOT}/opt/gcc/bin/gcc &&
 true
 
 %clean
 rm -rf ${RPM_BUILD_ROOT}
 
-
 %files
-%attr(0555,root,root) /usr/bin/gcc
-%attr(0755,root,root) /usr/install
-
+/opt/gcc
